0c024fe03bbddbe6240554ee73a2d462543acacc
mai <e36934@jp.ibm.com>
2019-09-16 11:33:33 +0900

    fix <Form>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Form from "./components/Form";
+// import Form from "./components/Form";
 import List from "./components/List";
 import './App.scss';
 import icon from './icon.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     // this.handleChange = this.handleChange.bind(this);
-    // this.fetchTasks = this.fetchTasks.bind(this);
+    this.fetchTasks = this.fetchTasks.bind(this);
     this.changeText = this.changeText.bind(this);
     this.addMemo = this.addMemo.bind(this);
     this.state = {
@@ -17,29 +17,25 @@ class App extends React.Component {
     };
   }
 
-  async componentDidMount(){
-    await fetch("http://localhost:3001/tasks") // データを取得しに行く
-    .then((response) => response.json()) // json型のレスポンスをオブジェクトに変換する
-    .then((json) => { // オブジェクトに変換したレスポンスを受け取り、
-      this.setState({ 
-        tasks: json
-     }); // Stateを更新する
-    })
-    .catch((error) =>{
-      console.error(error);
-    });
+  componentDidMount() {
+    this.fetchTasks()
   }
 
-  changeText = (event)=> {
-    console.log("bbb");
+  async fetchTasks() {
+    fetch("http://localhost:3001/tasks") // データを取得しに行く
+      .then(response => response.json()) // json型のレスポンスをオブジェクトに変換する
+      .then(json => { // オブジェクトに変換したレスポンスを受け取り、
+        this.setState({ tasks: json }) // Stateを更新する
+      })
+  }
+
+  changeText = (event) => {
     const inputText = event.target.value
     this.setState({ inputText: inputText })
-    console.dir(inputText);
   }
 
   addMemo = () => {
-    console.log("aaa");
-   fetch("http://localhost:3001/tasks", {
+    fetch("http://localhost:3001/tasks", {
       method: "POST",
       headers: {
         'Accept': 'application/json',
@@ -47,25 +43,35 @@ class App extends React.Component {
       },
       body: JSON.stringify({ content: this.state.inputText })
     })
-    .then( this.fetchTasks )
+      .then(this.fetchTasks)
   }
-    deleteMemo(taskId) {
-      fetch("http://localhost:3001/tasks/"+taskId, {
-        method: "DELETE"
-      })
-      .then( this.fetchTasks )
-    }
+  deleteMemo(taskId) {
+    fetch("http://localhost:3001/tasks/" + taskId, {
+      method: "DELETE"
+    })
+      .then(this.fetchTasks)
+  }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <div className="header">
           <i className="material-icons">menu</i>
-          <img src={icon} className="icon" alt=""/>
+          <img src={icon} className="icon" alt="" />
           <h2>Keep</h2>
-          
         </div>
-        <Form value={this.state.inputText} onClick={this.addMemo} onChange={this.changeText} />
+        <div className="form">
+          <div className="form-top">
+            <input className="input-box" placeholder="メモを入力..." 　onChange={this.changeText} />
+          </div>
+          <div className="form-bottom">
+            <i className="material-icons">color_lens</i>
+            <i className="material-icons">label</i>
+            <button className="input-close" type="submit" onClick={this.addMemo}>作成</button>
+          </div>
+        </div>
+        }
+        {/* <Form value={this.state.inputText} onClick={this.addMemo} onChange={this.changeText} /> */}
         <List tasks={this.state.tasks} onClick={this.updateMemo} deleteMemo={this.deleteMemo} />
       </div>
     );
